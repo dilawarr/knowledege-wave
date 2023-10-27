c76218f9bb6e3163d322795fc049c8baeae80aac
Dilawar Khan <dilawar2030@gmail.com>
2023-10-27 20:56:32 +0500

    Added togglable search button

---
+++ js/global.js
@@ -0,0 +1,8 @@
+function toggleSearch() {
+
+    let elem = document.getElementById("searchBar");
+    if(elem.style.display == "none")
+        elem.style.display = "block";
+    else
+        elem.style.display = "none";
+}
